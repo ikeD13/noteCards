faeb6ad37f5fe4b04a084323c4df10113f05e976
Galvanize Web Development <Galvanize@Isaacs-MacBook-Pro.local>
2019-01-26 15:51:05 -0700

    even more debugging

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const env = process.env.NODE_ENV || "development";
 const config = require('./knexfile.js')[env];
 const knex = require('knex')(config);
